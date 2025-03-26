4f064d9bbbda8ab384dd8cd534ff569e5e9ec80b
Asier <alumno@gmail.com>
2025-03-26 19:34:02 +0100

    Navegación con router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <DomusPagina />
+  <!-- <DomusPagina /> -->
+   <main class="bg-slate-100">
+    <nav>
+      <RouterLink to="#/">Home</RouterLink>
+      <RouterLink to="#/features">Features</RouterLink>
+      <RouterLink to="#/pricing">Pricing</RouterLink>
+      <RouterLink to="#/contact">Contact</RouterLink>
+
+    </nav>
+
+    <RouterView />
+   </main>
 </template>
 
 <script setup lang="ts">
-import DomusPagina from './modulorum/landing/pagimae/DomusPagina.vue';
+import { RouterView } from 'vue-router';
+import router from './router';
+// import DomusPagina from './modulorum/landing/pagimae/DomusPagina.vue';
+
+
 
 
 </script>
---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import DomusPagina from '@/modulorum/landing/paginae/DomusPagina.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: DomusPagina,
+    },
+    {
+      path: '/features',
+      name: 'Features',
+      component: () => import('@/modulorum/landing/paginae/PeculiaritatesPagina.vue'),
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: () => import('@/modulorum/landing/paginae/PretiumPagina.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/modulorum/landing/paginae/ContactusPagina.vue'),
+    },
+  ],
+});
+
+export default router;
